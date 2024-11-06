ccb787db82a643500b09650f41ee57c90fa64a20
hugo.seleiro <hugo.seleiro@shieldfc.com>
2024-11-06 10:41:06 +0000

    Login page form creation

--- src/app/pages/authorization/login.ts
+++ src/app/pages/authorization/login.ts
@@ -1,13 +1,51 @@
-import {Component} from "@angular/core";
+import {Component, inject, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'login',
   template: `
-  <div>Login</div>
+    <div>Login</div>
+    <form [formGroup]="form">
+      <fieldset>
+        <legend>Login</legend>
+        <div class="form-field">
+          <label>Email:</label>
+          <input name="email" formControlName="email">
+        </div>
+        <div class="form-field">
+          <label>Password:</label>
+          <input name="password" formControlName="password" type="password">
+        </div>
+      </fieldset>
+      <div class="form-buttons">
+        <button class="button button-primary" (click)="login()">
+          Login
+        </button>
+      </div>
+    </form>
   `,
   standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
   providers: []
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
+  private readonly fb = inject(FormBuilder);
+
+  ngOnInit() {
+    this.form = this.fb.nonNullable.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    if (this.form.valid) {
+      console.log(this.form.value)
+    }
+  }
+  
 }
