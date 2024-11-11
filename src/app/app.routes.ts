a720bb2874cca70b9fe98180543b2316327ed954
hugo.seleiro <hugo.seleiro@shieldfc.com>
2024-11-11 20:04:15 +0000

    Add navigate to admin guard and logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/authorization/login";
 import {authGuard} from "./shared/guards/auth.guard";
+import {adminGuard} from "./shared/guards/admin.guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login',component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'dashboard', canActivate: [authGuard], loadComponent: async () => {
       const m = await import('./pages/dashboard.component');
       return m.DashboardComponent;
-    }
-  },
-  { path: 'admin', canActivate: [authGuard], loadComponent: async () => {
-    const m = await import('./pages/admin.component');
-    return m.AdminComponent
-    }}
+    },
+    children: [
+      { path: 'admin', canActivate: [adminGuard], loadComponent: async () => {
+          const m = await import('./pages/admin.component');
+          return m.AdminComponent;
+        }
+      }
+    ]
+  }
 ];
