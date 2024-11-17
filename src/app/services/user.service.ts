8517a2328b57705efcb6f43f26f6032bc28268cf
hseleiro <hseleiro@gmail.com>
2024-11-17 22:58:34 +0000

    #4 - Inject HttpClient service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,8 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
-export class UserService {}
+export class UserService {
+  private readonly http = inject(HttpClient);
+
+}
