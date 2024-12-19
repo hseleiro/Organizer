242d6e0bc9fe2c0156fc21b5e9874633b6147b39
hseleiro <hseleiro@gmail.com>
2024-12-19 20:17:03 +0000

    [Test] - Create resolver data service unit tests.

---
+++ src/app/services/resolver-data.service.spec.ts
@@ -0,0 +1,31 @@
+import { ResolverDataService } from './resolver-data.service';
+import {BehaviorSubject, of} from "rxjs";
+
+describe('ResolverDataService', () => {
+  let service: ResolverDataService;
+  let isAdminMock$: BehaviorSubject<boolean>;
+
+  beforeEach(() => {
+    isAdminMock$ = new BehaviorSubject<boolean>(false);
+
+    service = {
+      isAdmin$: of(true),
+      setAdminPermission: jest.fn((isAdmin: boolean) => {
+        isAdminMock$.next(isAdmin);
+      }),
+    } as unknown as ResolverDataService
+  });
+
+  it('should be successfully instantiated', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should set admin permission', (done) => {
+    service.setAdminPermission(true);
+
+      service.isAdmin$.subscribe((res) => {
+        expect(res).toBe(true);
+        done()
+      })
+  });
+});
